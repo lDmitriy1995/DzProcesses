827d3f5d1338146ce66d2f2a6335d97072f1080f
Admin <Admin@DESKTOP-QQR48TL>
2023-06-14 00:29:26 +0600

    Добавьте файлы проекта.

---
+++ DzProcesses/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static int Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Необходимо передать строку данных в аргументах командной строки.");
+            return 1;
+        }
+
+        string data = args[0];
+        string filePath = "output.txt";
+
+        try
+        {
+            File.WriteAllText(filePath, data);
+            Console.WriteLine("Запись файла выполнена успешно.");
+            return 0;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ошибка при записи файла: " + ex.Message);
+            return 1;
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string data = "Hello world";
+
+        Process process = new Process();
+        process.StartInfo.FileName = "путь_к_первой_программе.exe";
+        process.StartInfo.Arguments = data;
+        process.StartInfo.RedirectStandardOutput = true;
+        process.StartInfo.UseShellExecute = false;
+        process.StartInfo.CreateNoWindow = true;
+
+        process.Start();
+        process.WaitForExit();
+
+        int exitCode = process.ExitCode;
+
+        if (exitCode == 0)
+        {
+            Console.WriteLine("Запись файла выполнена успешно.");
+        }
+        else
+        {
+            Console.WriteLine("Запись файла не удалась.");
+        }
+    }
+}
